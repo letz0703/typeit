45f10e11e8a8fe0f7da278c820563dc170f38adb
letz0703 <letz0703@gmail.com>
2022-05-16 06:51:19 +0900

    typewriter-effect for react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from "react";
 import "./App.css";
-import TypeIt from "typeit-react"
+import Typewriter from "typewriter-effect"
 
 function App() {
-  return <div>
-    <TypeIt
-      onInit={(w) => {
-        w.
-
-      }}
-    />
-
-  </div>
-
-
+  return ( // https://www.npmjs.com/package/typewriter-effect
+    <div className="App">
+      <Typewriter
+        onInit={(type) => {
+          type
+            .typeString("Love and Free!")
+            .pauseFor(1000)
+            .deleteAll()
+            .typeString("Lets...")
+            .pauseFor(1000)
+            .deleteAll()
+            .callFunction(() => {
+              alert("welcome")
+            })
+            .typeString("!etz")
+            .start()
+        }}
+      />
+    </div>
+  );
 }
+
 export default App;
