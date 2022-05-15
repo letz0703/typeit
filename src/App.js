78b132a0746877f48f4ecbf9b44cfdab365d6adb
letz0703 <letz0703@gmail.com>
2022-05-16 05:43:10 +0900

    yarn install: typewriter-effect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TypeIt from "typeit-react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+    <TypeIt
+      onInit={(w) => {
+        w.
+
+      }}
+    />
+
+  </div>
+
+
 }
-
-
+export default App;
